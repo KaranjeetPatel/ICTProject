e2c4658a9ea123cb4a6fbd381dbe68f45d5d55d0
Niketan Balar <>
2024-10-25 19:50:53 +0300

    add home screen & splash to home flow

---
+++ ICTProject/ICTProject/loginVC.swift
@@ -0,0 +1,20 @@
+//
+//  loginVC.swift
+//  ICTProject
+//
+//  Created by Niketan Balar on 25/10/24.
+//
+
+import UIKit
+
+class loginVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    
+}
